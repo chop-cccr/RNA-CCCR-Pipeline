83c370e443c6e54b1237072e94caac8ff8e0029f
pamelamishra@gmail.com <mishrap@reslnvhpc053.research.chop.edu>
2025-10-27 13:48:05 -0400

    Made changes to paths

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-/mnt/isilon/cccr_bfx/CCCR_Pipelines/archive/Independent/nextflow run main_final.nf \
-    -c  /mnt/isilon/cccr_bfx/CCCR_Pipelines/RNA-Seq_Pipeline_HPC/nextflow.ALIGN.config \
-    --samplesheet /mnt/isilon/cccr_bfx/CCCR_Pipelines/RNA-Seq_Pipeline_HPC/assets/samplesheet.example.csv \
+    nextflow run main_final.nf \
+    -c  ./nextflow.ALIGN.config \
+    --samplesheet ./assets/samplesheet.example.csv \
     --read_group TEST  \
     --outdir /mnt/isilon/cccr_bfx/CCCR_Pipelines/RNA-Seq_Pipeline_HPC/example/TEST_HPC_runs  \
-    --genomeDir /mnt/isilon/cccr_bfx/CCCR_Pipelines/RNA-Seq_Pipeline_HPC/reference/Mouse/Reference/mm10_gencode/mm10STAR/  \
-    --rsem_ref /mnt/isilon/cccr_bfx/CCCR_Pipelines/RNA-Seq_Pipeline_HPC/reference/Mouse/rsem_index/rsem_mm10  \
+    --genomeDir ./reference/Mouse/Reference/mm10_gencode/mm10STAR/  \
+    --rsem_ref ./reference/Mouse/rsem_index/rsem_mm10  \
     --threads 10 -profile singularity 
 
