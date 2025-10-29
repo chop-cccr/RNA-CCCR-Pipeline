e4c71ebddbbd8779a659e50ca49eb79988edb542
pamelamishra@gmail.com <mishrap@reslnvhpc053.research.chop.edu>
2025-10-29 12:10:12 -0400

    changes to run_nextflow.sh

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -5,7 +5,7 @@
     -c  ./nextflow.ALIGN.config \
     --samplesheet ./assets/samplesheet.example.csv \
     --read_group TEST  \
-    --outdir /mnt/isilon/cccr_bfx/CCCR_Pipelines/RNA-Seq_Pipeline_HPC/example/TEST_HPC_runs  \
+    --outdir ./TEST_HPC_runs  \
     --genomeDir ./reference/Mouse/Reference/mm10_gencode/mm10STAR/  \
     --rsem_ref ./reference/Mouse/rsem_index/rsem_mm10  \
     --threads 10 -profile singularity 
